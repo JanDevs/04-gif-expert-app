98e8fb5ad0ba5b9696f0db2fb898d756ced64600
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-07-11 11:46:08 -0600

    Componente AddCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react"
+import AddCategory from "./components/AddCategory";
 
 const GifExpertApp = () => {
 
     const [categories, setCategories] = useState([ 'One punch', 'Dragon ball' ]);
 
     const onAddCategory = () =>{
+        
         setCategories([...categories, 'Nueva categoría']);
     }
 
     return (
         <>
             <h1>GifExpert App</h1>
+            <AddCategory />
             <button onClick={ onAddCategory }>Agregar</button>
             <ol>
                 { categories.map( category => {
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const AddCategory = () => {
+
+    const [ inputValue, setInputValue ] = useState('');
+
+    //Si se pone solo la función en el onChange, ejecuta la función con el evento como argumento
+    const onInputChange = ( event ) => {
+        setInputValue( event.target.value );
+    }
+
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+        console.log(inputValue);
+    }
+
+
+    return (
+        <form onSubmit={ onSubmit } >
+            <input
+            type='text'
+            placeholder='Buscar gifs'
+            value={ inputValue }
+            onChange={  onInputChange }
+            />
+        </form>
+        
+    )
+}
+
+export default AddCategory
