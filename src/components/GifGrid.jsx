59d47a64d159aa057586075edfce82df033cee39
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-07-15 10:25:09 -0600

    useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,23 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs"
 
 
 
 const GifGrid = ({ category }) => {
   
+    const [counter, setCounter] = useState(10);  
     
+    useEffect(() => {
+        getGifs( category );
+    }, [  ])
+
     
-    getGifs( category );
 
     return (
-    <>
-        <h3>{ category }</h3>
-    </>
+        <>
+            <h3>{ category }</h3>
+            <h5>{ counter }</h5>
+        </>
   )
 }
 
