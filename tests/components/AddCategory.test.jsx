df0b9368beea5ca43a6dc74773cf6f4e19497eae
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-07-31 11:55:22 -0600

    Jest functions

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -13,8 +13,9 @@ describe('Pruebas en AddCategory', () => {
     test('Debe de llamar onNewCategory si el input tiene un valor', () => {
         
         const inputValue = 'Saitama';
-        //TODO: 
-        render( <AddCategory onNewCategory={ () => {} } /> );
+        const onNewCategory = jest.fn();
+
+        render( <AddCategory onNewCategory={ onNewCategory } /> );
 
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
@@ -25,6 +26,28 @@ describe('Pruebas en AddCategory', () => {
 
         expect( input.value ).toBe('');
 
+        expect( onNewCategory ).toHaveBeenCalled();
+        //expect( onNewCategory ).toHaveBeenCalledTimes(1);
+        expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+
+
+    });
+
+    test('No debe de llamar el onNewCategory si el input está vacío', () => {
+      
+        const onNewCategory = jest.fn();
+
+        render( <AddCategory onNewCategory={ onNewCategory } /> );
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+        
+        expect( input.value ).toBe('');
+        fireEvent.submit( form );
+
+        expect( onNewCategory ).toHaveBeenCalledTimes(0);
+
     })
+    
     
 })
