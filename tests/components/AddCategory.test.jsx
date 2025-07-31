97987f3c3a579a7fa1b124e494684cd7cae951d4
Jan Luis Juárez Avalos <janelproxd@gmail.com>
2025-07-31 11:42:14 -0600

    Pruebas en AddCategory, simular submit

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -10,5 +10,21 @@ describe('Pruebas en AddCategory', () => {
         expect( input.value ).toBe('Saitama');
     })
     
+    test('Debe de llamar onNewCategory si el input tiene un valor', () => {
+        
+        const inputValue = 'Saitama';
+        //TODO: 
+        render( <AddCategory onNewCategory={ () => {} } /> );
 
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input( input, { target: { value: inputValue } } );
+
+        fireEvent.submit( form );
+
+        expect( input.value ).toBe('');
+
+    })
+    
 })
